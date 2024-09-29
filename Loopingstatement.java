45a70b8389bc6c1854ed1e5bb6c2649b88a08f23
Twinkle Singh <169064326+twinkleT11singh@users.noreply.github.com>
2024-09-29 12:47:59 +0530

    Add files via upload

---
+++ Loopingstatement.java
@@ -0,0 +1,25 @@
+package hello;
+import java.util.*;
+public class Loopingstatement {
+
+	public static void main(String[]args)
+	{
+	  //do while,while,for,for-each();
+		//for(intialization;condition;increment){
+		
+//		FIND A EVEN NO. BETWEEN 1 TO 10 BY USING FOR LOOP
+		
+		int count=1;
+		for( count=1;count<=10;count++)
+		{
+			if(count%2==0) {
+				System.out.println(count);
+			}
+			
+		}
+		
+	}
+
+}
+
+ 
